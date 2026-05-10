057be3d2b1074a3d2bba9e88d28f908b3b76ed11
abhisheksth9 <abhisheksth7@gmail.com>
2026-05-10 22:11:32 +0545

    fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,24 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
-const dashboardRoutes = require("./routes/dashboardRoutes")
-const incomeRoutes = require("./routes/incomeRoutes")
-const expenseRoutes = require("./routes/expenseRoutes")
+const dashboardRoutes = require("./routes/dashboardRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mern-expense-tracker-nz75.vercel.app",
+];
+
 app.use(
-    cors({
-        origin: "https://mern-expense-tracker-nz75.vercel.app",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
 
 app.use(express.json());
@@ -23,10 +29,13 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/dashboard", dashboardRoutes)
-app.use("/api/v1/income", incomeRoutes)
-app.use("/api/v1/expense", expenseRoutes)
-app.use("/uploads", express.static(path.join(__dirname, 'uploads')))
+app.use("/api/v1/dashboard", dashboardRoutes);
+app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
