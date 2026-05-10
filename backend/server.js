b48107a4f5fb2f90fadcb53668b3b27d3806f513
abhisheksth9 <abhisheksth7@gmail.com>
2026-05-10 18:54:32 +0545

    fix API URL

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.CLIENT_URL || "*",
+        origin: 'https://mern-expense-tracker-5n0o.onrender.com',
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
--- frontend/src/utils/apiPath.js
+++ frontend/src/utils/apiPath.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000/";
+export const BASE_URL = "https://mern-expense-tracker-5n0o.onrender.com";
+
 
 export const API_PATHS = {
     AUTH: {
